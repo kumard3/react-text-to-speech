77c0890bd5beec0c65ca9177d13a1f0692fdc0b3
kumard3 <kumardeepanshu157200@gmail.com>
2022-01-31 01:16:02 +0530

    web api updated

--- src/pages/WebApi.tsx
+++ src/pages/WebApi.tsx
@@ -1,29 +1,26 @@
-import { useState, useEffect, useCallback } from "react";
-import { textarea, button, layout } from "../styles/style";
+import { useState, useCallback, useEffect } from "react";
 
 export default function WebApi() {
   const [value, setValue] = useState(
     "Hey there! Cyphen, a community  with great passion and zest in building a healthy environment To learn •Open Source•Web Development•Data structures and Algorithms•Projects•Competitive Coding•ProgrammingMotivational talks, Opportunities, and much much more!Suggestions are always welcomed."
   );
-  const [speekvalue, setSpeekValue] = useState("");
+
   const [isSpeaking, setIsSpeaking] = useState(false);
-  const [isCanceled, setIsCanceled] = useState(false);
   const [isPaused, setIsPaused] = useState(false);
   const [isResumed, setIsResumed] = useState(false);
   const [isEnded, setIsEnded] = useState(false);
-  var msg = new SpeechSynthesisUtterance();
 
-  msg.text = value;
+  const speak = useCallback(() => {
+    var msg = new window.SpeechSynthesisUtterance();
 
-  const createGraphics = useCallback(() => {
+    msg.text = value;
     function speak() {
       window.speechSynthesis.speak(msg);
     }
     speak();
     setIsSpeaking(true);
-    setIsEnded(false)
-   
-  }, [msg]);
+    setIsEnded(false);
+  }, []);
 
   const Pause = useCallback(() => {
     function pause() {
@@ -32,11 +29,10 @@ export default function WebApi() {
     pause();
     setIsPaused(true);
     setIsSpeaking(false);
-    setIsEnded(false)
-    setIsResumed(false)
-   
-  }, []); 
-  
+    setIsEnded(false);
+    setIsResumed(false);
+  }, []);
+
   const Resume = useCallback(() => {
     function resume() {
       window.speechSynthesis.resume();
@@ -44,30 +40,28 @@ export default function WebApi() {
     resume();
     setIsPaused(false);
     setIsSpeaking(false);
-    setIsEnded(false)
-    setIsResumed(true)
-   
-  }, []); 
-  
+    setIsEnded(false);
+    setIsResumed(true);
+  }, []);
+
   const Cancel = useCallback(() => {
     function cancel() {
       window.speechSynthesis.cancel();
     }
     cancel();
     setIsPaused(false);
-    setIsResumed(false)
+    setIsResumed(false);
 
     setIsSpeaking(false);
-    setIsEnded(true)
+    setIsEnded(true);
   }, []);
 
-  
+  const textarea = `bg-gray-500 text-white rounded-3xl p-[14px] text-xl w-[360px] sm:w-[30rem] min-h-[20rem]`;
 
-
- 
+  const button = `text-white rounded-xl bg-gray-500 px-5 py-3 text-xl`;
 
   return (
-    <div className={`${layout}`}>
+    <div className="flex flex-col mt-10 justify-center  items-center">
       <div className="max-w-[36rem]">
         <textarea
           className={`${textarea} `}
@@ -76,16 +70,10 @@ export default function WebApi() {
         />
       </div>
       <div className="w-full flex justify-center items-center">
-        {/* {paused || pending === !true ? "Resume" : "Paused"} */}
-
-        {/* { window.speechSynthesis.paused === false ? `paused: true
-pending: false
-speaking: false` : ``} */}
-
-        <button onClick={createGraphics} className={`${button} mx-3 `}>
+        <button onClick={speak} className={`${button} mx-3 `}>
           Speak
         </button>
-{isSpeaking ? "Speaking" : ""}
+        {isSpeaking ? "Speaking" : ""}
         <button onClick={Pause} className={`${button} mx-3 `}>
           pause
         </button>
@@ -97,7 +85,7 @@ speaking: false` : ``} */}
         <button onClick={Cancel} className={`${button} mx-3 active:  `}>
           cancel
         </button>
-        {isCanceled ? "Canceled" : ""}
+        {isEnded ? "Canceled" : ""}
       </div>
     </div>
   );
