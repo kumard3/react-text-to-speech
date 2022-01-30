7cd57e041ba39ebe4faf53e4cf8d57eff9cc1470
kumard3 <kumardeepanshu157200@gmail.com>
2022-01-30 17:12:26 +0530

    style changed

--- src/styles/style.ts
+++ src/styles/style.ts
@@ -1,3 +1,3 @@
-export const  textarea = `bg-gray-500 text-white rounded-3xl w-[360px] sm:w-[30rem] min-h-[20rem]`
+export const  textarea = `bg-gray-500 text-white rounded-3xl p-[14px] text-xl w-[360px] sm:w-[30rem] min-h-[20rem]`
 
 export const  button = `text-white rounded-xl bg-gray-500 px-5 py-3 text-xl`
